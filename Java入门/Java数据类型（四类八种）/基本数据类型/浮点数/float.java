2d948e4240480422aa2bb52a7510e787c73ceb76
NGB_Kio <86999728+NGBKio@users.noreply.github.com>
2025-03-29 11:32:17 +0800

    Create float.java

---
+++ Java入门/Java数据类型（四类八种）/基本数据类型/浮点数/float.java
@@ -0,0 +1,8 @@
+        // float
+        /* 定义float类型变量也需要加一个F作为后缀 */
+        float f = 10.1F;
+        System.out.println(f);
+
+        // double
+        double e = 20.2;
+        System.out.println(e);
