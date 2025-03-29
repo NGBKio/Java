6a1ac68004dd166c8cd2e81840936bc180e2f3b6
NGB_Kio <86999728+NGBKio@users.noreply.github.com>
2025-03-29 11:28:52 +0800

    Create VariableDemo.java

---
+++ Java入门/Java数据类型（四类八种）/基本数据类型/整数/VariableDemo.java
@@ -0,0 +1,30 @@
+/* Java基本数据类型 —— 整数类型
+* 整数又分为byte(字节)、short、int、long
+*/
+
+public class VariableDemo{
+    public static void main(String[] args){
+        // 整数
+        // byte
+        byte b = 10;
+        System.out.println(b);
+
+        // short
+        short s = 20;
+        System.out.println(s);
+
+        // int
+        int i = 30;
+        System.out.println(i);
+
+        //long
+        /*long类型的变量 需要在数据值后面加一个L作为后缀
+        否则会报错
+        后缀可以是大写 也可以是小写
+        建议使用大写 */
+        long x = 9999999999999L;
+        System.out.println(x);
+
+
+    }
+}
